d767b872ec2da30c67d72e047458d96df932e93f
enver gülüm <envergulum96@gmail.com>
2022-09-16 14:27:11 +0300

    second

---
+++ operating.py
@@ -0,0 +1,10 @@
+class processes:
+    def addtion_process(a,b):
+        return a+b
+    def subtraction_process(a,b):
+        return a-b
+    def multiplication_process(a,b):
+        return a*b
+    def division_process(a,b):
+        return a/b
+
